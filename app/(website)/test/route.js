316249bc13cc000e1c5fcfb7d481114a9572da01
Maciej Malinowski <mejsiejdev@gmail.com>
2024-05-29 11:00:39 +0200

    Update route.js

--- app/(website)/test/route.js
+++ app/(website)/test/route.js
@@ -116,9 +116,175 @@ export async function GET() {
         };
       });
 
-    console.log("Profile fields choices: ", profile_fields_choices);
+    const tags = convert
+      .xml2js(data.slice(data.indexOf("<tags>"), data.indexOf("</tags>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .tags.tag.map(({ _attributes, name }) => {
+        return {
+          id: _attributes.id,
+          name: name?.map(({ _attributes, _cdata }) => {
+            return {
+              lang: _attributes.lang,
+              text: _cdata,
+            };
+          }),
+        };
+      });
 
-    //console.log(data.slice(data.indexOf("</profile_fields>") + 1, data.indexOf("</profile_fields>") + 100));
+    const tariff_strategies = convert
+      .xml2js(data.slice(data.indexOf("<tariff_strategies>"), data.indexOf("</tariff_strategies>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .tariff_strategies.strategy.map(({ _attributes }) => {
+        return {
+          id: _attributes.id,
+          name: _attributes.name,
+          currency: _attributes.currency,
+        };
+      });
+
+    const comparers = convert
+      .xml2js(data.slice(data.indexOf("<comparers>"), data.indexOf("</comparers>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .comparers.comparer.map(({ _attributes }) => {
+        return {
+          id: _attributes.id,
+          name: _attributes.name,
+        };
+      });
+
+    const producers = convert
+      .xml2js(data.slice(data.indexOf("<producers>"), data.indexOf("</producers>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .producers.producer.map(({ _attributes }) => {
+        return {
+          id: _attributes.id,
+          name: _attributes.name,
+        };
+      });
+
+    // products by themselves are too long
+    // I will have to save them to Postgres one by one...
+
+    // why do I have to do this
+    const endIndexes = data.slice(0, data.indexOf("</products>")).reduce(function (a, e, i) {
+      if (e === "</product>") {
+        a.push(i);
+      }
+      return a;
+    }, []);
+
+    let start = data.indexOf("<products>") + 1;
+
+    endIndexes.forEach((i) => {
+      const product = convert.xml2js(data.slice(start, i + 1), { compact: true, spaces: 2 });
+      delete product._text;
+      console.log(product);
+      start = i + 1;
+    });
+
+    /*
+    const stockTotal = convert
+      .xml2js(data.slice(data.indexOf("<stockTotal>"), data.indexOf("</stockTotal>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .stockTotal.stock.map(({ _attributes }) => {
+        return {
+          alias: _attributes.alias,
+          quantity: _attributes.quantity,
+        };
+      });
+
+    const titles = convert
+      .xml2js(data.slice(data.indexOf("<titles>"), data.indexOf("</titles>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .titles.title.map(({ _attributes, _cdata }) => {
+        return {
+          lang: _attributes.lang,
+          text: _cdata,
+        };
+      });
+
+    const descriptions = convert
+      .xml2js(data.slice(data.indexOf("<descriptions>"), data.indexOf("</descriptions>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .descriptions.description.map(({ _attributes, _cdata }) => {
+        return {
+          alias: _attributes.alias,
+          lang: _attributes.lang,
+          text: _cdata,
+        };
+      });
+
+    const urls = convert
+      .xml2js(data.slice(data.indexOf("<urls>"), data.indexOf("</urls>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .urls.url.map(({ _attributes, _cdata }) => {
+        return {
+          alias: _attributes.alias,
+          lang: _attributes.lang,
+          text: _cdata,
+        };
+      });
+
+    const images = convert
+      .xml2js(data.slice(data.indexOf("<images>"), data.indexOf("</images>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .images.image.map(({ _attributes }) => {
+        return {
+          ordering: _attributes.ordering,
+          url: _attributes.url,
+        };
+      });
+*/
+    /*
+    const variants = convert
+      .xml2js(data.slice(data.indexOf("<variants>"), data.indexOf("</variants>") + 1), {
+        compact: true,
+        spaces: 2,
+      })
+      .variants.variant.map(({ _attributes, optionName, stockTotal, basePrice, sellPrice }) => {
+        return {
+          id: _attributes.id,
+          symbol: _attributes.symbol,
+          ean: _attributes.ean,
+          isActive: _attributes.isActive,
+          optionName: optionName?.map(({ _attributes, _cdata }) => {
+            return {
+              lang: _attributes.lang,
+              text: _cdata,
+            };
+          }),
+          stockTotal: stockTotal.map(({ stock }) => {
+            return {
+              alias: _attributes.alias,
+              quantity: _attributes.quantity,
+            };
+          }),
+          basePrice: basePrice,
+          sellPrice: sellPrice,
+        };
+      });
+      */
+    //console.log(variants);
+
+    //console.log(data.slice(data.indexOf("</images>") + 1, data.indexOf("</images>") + 100));
   });
 
   return Response.json({ data: [] });
